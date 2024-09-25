e6db1eb258174e2051a5d5a43f1f8da785b991c9
DEC-S042\daw1 <pablo.rivero.gutierrez@gmail.com>
2024-09-25 11:27:00 +0200

    Actividad 1

---
+++ Proyecto1/src/com/decroly/daw/Principal.java
@@ -0,0 +1,63 @@
+package com.decroly.daw;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		/*
+		 * //Ejercicio 1 System.out.println("EJERCICIO 1");
+		 * 
+		 * System.out.println("Buenos Dias");
+		 * 
+		 * //Ejercicio 2 System.out.println("\nEJERCICIO 2");
+		 * 
+		 * //Damos valor a las variables
+		 * 
+		 * int lado = 5;
+		 * 
+		 * //Calculamos el area
+		 * 
+		 * System.out.println("Area = 5*5"); System.out.print("Area =");
+		 * System.out.println(lado * lado);
+		 * 
+		 * //Ejercicio 3 System.out.println("EJERCICIO 3");
+		 * 
+		 * //Damos valor a las variables
+		 * 
+		 * Double lado3; Scanner entrada = new Scanner(System.in);
+		 * System.out.println("¿Cual es el lado del cuadrado?:"); lado3 =
+		 * entrada.nextDouble();
+		 * 
+		 * //Calculamos el area con los datos recogidos
+		 * 
+		 * System.out.print("El area del cuadrado es:"); System.out.println(lado3 *
+		 * lado3);
+		 * 
+		 * //Ejercicio 4 System.out.println("EJERCICIO 4");
+		 * 
+		 * //Damos valor a las variables
+		 * 
+		 * Double E4num1; Double E4num2; entrada = new Scanner(System.in);
+		 * System.out.println("Dame un numero:"); E4num1 = entrada.nextDouble();
+		 * System.out.println("Dame otro:"); E4num2 = entrada.nextDouble();
+		 * 
+		 * //Calculamos las operaciones
+		 * 
+		 * System.out.print("La suma es:"); System.out.println(E4num1 + E4num2);
+		 * 
+		 * System.out.print("La resta es:"); System.out.println(E4num1 - E4num2);
+		 * 
+		 * System.out.print("El producto es:"); System.out.println(E4num1 * E4num2);
+		 * 
+		 * System.out.print("La division es:"); System.out.println(E4num1 / E4num2);
+		 */
+	//Ejercicio 5
+		System.out.println("EJERCICIO 5");
+		
+	} 
+
+}
+
