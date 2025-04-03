9d916095725c143d6db317ecb3d61b5b990d3f95
Pablo <pablo.rivero.gutierrez@gmail.com>
2025-04-03 17:32:57 +0200

    act

--- ConexionBBDD/main/src/main/java/com/decroly/SQLDatabaseManager.java
+++ ConexionBBDD/main/src/main/java/com/decroly/SQLDatabaseManager.java
@@ -1,24 +1,41 @@
 package com.decroly;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLDatabaseManager {
-    
+
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/";
-    private static final String SCHEMA = "ConexionBBDD";
-    private static final String USUARIO = "root";
-    private static final String CLAVE = "daw12";
-//jdbc:mysql://127.0.0.1:3306/?user=root
-    public static Connection getConnection(){
+    private static String URL;
+    private static String SCHEMA;
+    private static String USUARIO;
+    private static String CLAVE;
+
+    static {
+        try (InputStream inputStream = SQLDatabaseManager.class.getClassLoader().getResourceAsStream("conexionBBDD.dat");
+             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            URL = br.readLine();
+            SCHEMA = br.readLine();
+            USUARIO = br.readLine();
+            CLAVE = br.readLine();
+
+        } catch (IOException | NullPointerException e) {
+            System.out.println("Error al leer el archivo de configuración: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
         Connection connection = null;
 
         try {
             Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL+SCHEMA, USUARIO, CLAVE);
-            
+            connection = DriverManager.getConnection(URL + SCHEMA, USUARIO, CLAVE);
         } catch (ClassNotFoundException e) {
             System.out.println("Error de acceso al driver: " + e.getMessage());
         } catch (SQLException e) {
