e0d81bf88fc99457ae41707fcb1343e99d3bde45
COMPU-S031\daw1 <pablo.rivero.gutierrez@gmail.com>
2025-04-16 11:22:33 +0200

    act

--- VideoDawBBDD/main/src/main/java/com/decroly/SQLAcessVideoDaw.java
+++ VideoDawBBDD/main/src/main/java/com/decroly/SQLAcessVideoDaw.java
@@ -36,4 +36,29 @@ public class SQLAcessVideoDaw {
         }
         return Peliculas;
     }
+
+    public List<Articulo> getVideojuegos(){//METODO PARA MOSTRAR TODOS LOS ARTICULOS
+        List<Articulo> Videojuegos = new LinkedList<>();
+
+        String getVideojuegos =   "SELECT a.cod, a.titulo, a.descripcion, a.fecha_registro, p.genero, p.alquilada FROM Articulo a JOIN Pelicula p ON p.cod = a.cod WHERE a.fecha_baja = '9999-01-01';";
+
+        try (Connection connection = SQLDatabaseManager.getConnection(); Statement statement = connection.createStatement();
+        ResultSet dataSet = statement.executeQuery(getVideojuegos);) {
+            while(dataSet.next()){
+                int cod = dataSet.getInt(1);
+                String titulo = dataSet.getNString(2);
+                String descripcion = dataSet.getNString(3);
+                Date fechaRegistro = dataSet.getDate(4);
+                String genero = dataSet.getNString(5);
+                boolean alquilada = dataSet.getBoolean(6);
+
+                Articulo p1 =  new Pelicula(cod, titulo, descripcion, fechaRegistro, genero, alquilada);
+                Videojuegos.add(p1);
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return Videojuegos;
+    }
 }
